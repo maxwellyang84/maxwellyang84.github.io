c764cd96ca3213ab8586e30ec7d90eeb41c2889a
Maxwell Yang <maxwellyang84@gmail.com>
2020-04-02 01:04:37 -0700

    added fade in when pressing home

--- js/aboutFade.js
+++ js/aboutFade.js
@@ -14,3 +14,17 @@ function fadeOutMain(){
    
     
 }
+
+function fadeInMain(){
+    var element = document.getElementById("mainpage");
+    var i = 0;
+    var k = window.setInterval(function(){
+        if(i > 10){
+            clearInterval(k);
+        }else{
+            element.style.opacity = i/10;
+            i++;
+        }
+    }, 100);
+    element.style.pointerEvents = "auto";
+}
