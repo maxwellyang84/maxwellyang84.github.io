cff2b4296625e5f8a64940868ca1c26f0cdf4456
Maxwell Yang <maxwellyang84@gmail.com>
2020-04-04 01:04:46 -0700

    changed background image and fixed js

--- js/myScript.js
+++ js/myScript.js
@@ -1,8 +1,10 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopNav");
   if (x.className === "topnav") {
+      console.log("here")
     x.className += " responsive";
   } else {
+      console.log("nope here")
     x.className = "topnav";
   }
 }
